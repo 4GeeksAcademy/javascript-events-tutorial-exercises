5b073dcc932be9fbe42c33c46765f14f5b23400d
Alejandro Sanchez <aalejo@gmail.com>
2021-02-27 11:31:12 -0500

    Update index.js

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -1,4 +1,5 @@
-window.onLoad = function calculateSumListener(){
+// adding the function to the window makes it globally available
+window.calculateSumListener = function() {
 	//Return the value of the input #firstNumber
 	var stringA = document.getElementById("firstNumber").value;
 	//Return the value of the input #secondNumber
