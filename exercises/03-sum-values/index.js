463ef7adde2ddb2fef03abbadd21e124df710bc4
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-20 23:05:45 +0200

    Update index.js

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -1,9 +1,9 @@
-// adding the function to the window makes it globally available
+// Adding the function to the window makes it globally available
 window.calculateSumListener = function() {
-	//Return the value of the input #firstNumber
-	var stringA = document.getElementById("firstNumber").value;
-	//Return the value of the input #secondNumber
-	var stringB = document.getElementById("secondNumber").value;
-
-	//your code goes here
+	// Return the value of the input #firstNumber
+	let stringA = document.getElementById("firstNumber").value;
+	// Return the value of the input #secondNumber
+	let stringB = document.getElementById("secondNumber").value;
+	// Your code here
+	
 };
