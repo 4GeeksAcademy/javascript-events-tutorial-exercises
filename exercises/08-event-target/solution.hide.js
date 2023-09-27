133656061c4dce489da844102e7f0272cdcc0554
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 15:19:10 +0200

    Create solution.hide.js

---
+++ exercises/08-event-target/solution.hide.js
@@ -0,0 +1,7 @@
+window.onload = function loadFn() {
+	let containerElm = document.getElementById("container");
+	containerElm.addEventListener("click", function(event) {
+		// Your code here
+        alert(event.target.id)
+	});
+};
