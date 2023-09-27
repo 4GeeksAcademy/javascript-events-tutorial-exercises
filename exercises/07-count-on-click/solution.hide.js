eb5b924184fa71cce51f45c8cc7e6e3ced064482
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 14:32:16 +0200

    Create solution.hide.js

---
+++ exercises/07-count-on-click/solution.hide.js
@@ -0,0 +1,26 @@
+// This is a global variable
+let counter = 0;
+
+window.onload = function loadFunction()
+{
+	// Here I set the screen to the initial value when the website is fully loaded
+	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+}
+
+// Called when the user clicks
+window.increaseCounter = function increaseCounter()
+{
+	// Increase the global counter by one
+	counter++;
+	// Update the screen with the new value
+	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+}
+
+// Your code here
+window.decreaseCounter = function decreaseCounter() 
+{
+    // Decrease the global counter by one
+    counter--;
+    // Update the screen with the new value
+	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+}
