c7febea2e40ff74f8e13a1fa1d063c6589e67c3f
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 14:53:05 +0200

    Create solution.hide.js

---
+++ exercises/07.1-change-turn-on-click/solution.hide.js
@@ -0,0 +1,18 @@
+let currentUser = "Mario";
+
+window.onload = function loadfn() {
+	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
+}
+
+// Modify this function
+window.turnChanger = function turnChanger() {
+	if (currentUser == "Mario") {
+		currentUser = "Juan";
+	} else if (currentUser == "Juan") {
+		currentUser = "Josh";
+	} else {
+        currentUser = "Mario";
+  }
+
+	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
+}
