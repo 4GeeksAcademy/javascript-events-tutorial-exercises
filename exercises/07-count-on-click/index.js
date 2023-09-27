253d3f4ec420bce596dfec909c6a636c5add6419
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 14:29:19 +0200

    Update index.js

--- exercises/07-count-on-click/index.js
+++ exercises/07-count-on-click/index.js
@@ -1,17 +1,17 @@
-//This is a global variable
-var counter = 0;
+// This is a global variable
+let counter = 0;
 
 window.onload = function loadFunction()
 {
-	//here I set the screen to the initial value when the website is fully loaded.
+	// Here I set the screen to the initial value when the website is fully loaded
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
 }
 
-//called when the user clicks
+// Called when the user clicks
 window.increaseCounter = function increaseCounter()
 {
-	//increase the global counter in one
+	// Increase the global counter by one
 	counter++;
-	//update the screen with the new value
+	// Update the screen with the new value
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
 }
